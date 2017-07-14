b040f108ed3ad3da09d15c4e9832bdd924dfed52
redchenjs <redchenjs@live.com>
2017-07-14 15:34:28 +0800

    更新代码注释

--- src/main.c
+++ src/main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * main.c
  *
@@ -20,26 +19,3 @@ int main(void)
         system_suspend();   // 系统休眠
     }
 }
-=======
-#include "inc/system/init.h"
-#include "inc/system/user.h"
-/*
- * main.c
- *
- *  Created on: 2017-4-16
- *      Author: redchenjs
- */
-
-int main(void)
-{
-    system_init();          //系统初始化
-
-    user_init();            //应用初始化
-
-    while (1) {
-        user_loop();        //应用循环
-
-        system_suspend();           //系统暂停
-    }
-}
->>>>>>> 41baafb782ec9eb247f2dc3f13689621aa1010c7
